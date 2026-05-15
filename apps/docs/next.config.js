13e8dd19c63591d284092f00724f122faca9dcf4
GeorgeCht <2hardforutoo@hotmail.gr>
2026-05-15 11:01:44 +0300

    chore: docs next config update for images

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -6,7 +6,19 @@ const withNextra = nextra({
   // Nextra-specific options
 });
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.brandfetch.io",
+      },
+    ],
+  },
+};
 
 export default withNextra({
   ...nextConfig,
